3fee56004f332d16bbe7fd99f6d35d5f8807af7a
Santiago Gomez <tafu2502@gmail.com>
2026-06-16 18:59:26 -0300

    agregamos id a cada usuario #18

--- js/usuarios.js
+++ js/usuarios.js
@@ -1,18 +1,17 @@
 let bodyTabla;
 window.addEventListener("load", function () {
-   
   bodyTabla = document.getElementById("bodyTabla");
   inicializacion();
   mostrarMovimiento();
 });
 function inicializacion() {
   const botonValidar = document.getElementById("botonValidar");
-  const botonSesion = document.getElementById("botonSesion")
-  if (botonValidar)
-   {botonValidar.addEventListener("click", validarDatos);}
-  if(botonSesion)
-    {botonSesion.addEventListener("click",inicioSesion);
-
+  const botonSesion = document.getElementById("botonSesion");
+  if (botonValidar) {
+    botonValidar.addEventListener("click", validarDatos);
+  }
+  if (botonSesion) {
+    botonSesion.addEventListener("click", inicioSesion);
   }
 }
 
@@ -21,7 +20,6 @@ function validarDatos() {
   const contraseñaCrear = document.getElementById("inputCrearContraseña");
 
   const divValidacion = document.getElementById("divValidacion");
-
 
   const nombre = nombreCrear.value;
   const contraseña = contraseñaCrear.value;
@@ -33,13 +31,13 @@ function validarDatos() {
   }
 }
 function validarUsuario(nombre, contraseña) {
-  const cuenta = { resultado: true, fallos: []};
+  const cuenta = { resultado: true, fallos: [] };
 
   if (nombre.length < 4) {
     cuenta["resultado"] = false;
     cuenta.fallos.push("El nombre de usuario debe tener al menos 4 letras ");
   }
-  if (contraseña.length<4 || contraseña.length > 10) {
+  if (contraseña.length < 4 || contraseña.length > 10) {
     cuenta["resultado"] = false;
     cuenta.fallos.push(
       "La contraseña del usuario debe tener entre 4 y 10 caracteres",
@@ -49,39 +47,43 @@ function validarUsuario(nombre, contraseña) {
 }
 
 function crearUsuario(nombreUsuario, contraseña) {
-  const usuario =  {
+  let id = 1;
+  const usuario = {
     nombre: nombreUsuario,
     contraseña: contraseña,
     estado: "habilitado",
     rol: "Usuario",
+    id: id,
   };
- return usuario;
+  id++;
+  return usuario;
 }
 function inicioSesion() {
   const inputNombre = document.getElementById("FloatInputNombre");
   const inputContraseña = document.getElementById("FloatInputContraseña");
-  const nombreUsuario = inputNombre.value
-  const contraseñaUsuario = inputContraseña.value
+  const nombreUsuario = inputNombre.value;
+  const contraseñaUsuario = inputContraseña.value;
 
-  let encontrado = false
+  let encontrado = false;
   const usuariosDelArray = JSON.parse(localStorage.getItem("usuarios"));
 
-     for (let i = 0; i <= usuariosDelArray.length - 1; i++) { 
-
-      if(nombreUsuario === usuariosDelArray[i].nombre && contraseñaUsuario === usuariosDelArray[i].contraseña){
-        encontrado = true
-        break;
-      }
+  for (let i = 0; i <= usuariosDelArray.length - 1; i++) {
+    if (
+      nombreUsuario === usuariosDelArray[i].nombre &&
+      contraseñaUsuario === usuariosDelArray[i].contraseña
+    ) {
+      encontrado = true;
+      break;
+    }
   }
 
-if(encontrado){
-  window.alert("Se encontro")
-}else{
-  window.alert("no se encontro")
+  if (encontrado) {
+    window.alert("Se encontro");
+  } else {
+    window.alert("no se encontro");
+  }
+  encontrado = false;
 }
-encontrado=false;
-}
-
 
 function agregarUsuarioLocalStorage(usuario) {
   const usuariosString = JSON.parse(localStorage.getItem("usuarios")) || [];
@@ -96,7 +98,7 @@ function mostrarMovimiento() {
   for (let i = 0; i <= usuariosDelArray.length - 1; i++) {
     let indice = i;
     const tr = document.createElement("tr");
-
+    const td_id = document.createElement("td");
     const td_1 = document.createElement("td");
     const td_2 = document.createElement("td");
     const td_3 = document.createElement("td");
@@ -104,6 +106,7 @@ function mostrarMovimiento() {
     const td_5 = document.createElement("td");
     const td_6 = document.createElement("td");
 
+    td_id.textContent = usuariosDelArray[i].id;
     td_1.textContent = usuariosDelArray[i].nombre;
     td_2.textContent = usuariosDelArray[i].contraseña;
     td_3.textContent = usuariosDelArray[i].estado;
@@ -111,6 +114,7 @@ function mostrarMovimiento() {
     td_5.innerHTML =
       '<Button class="btn btn-success mx-2"><i class="bi bi-check-square"></i></Button><Button class="btn btn-danger mx-2"><i class="bi bi-ban"></i></Button><Button class="btn btn-info mx-2"><i class="bi bi-clipboard-x"></i></Button>';
 
+    tr.appendChild(td_id);
     tr.appendChild(td_1);
     tr.appendChild(td_2);
     tr.appendChild(td_3);
