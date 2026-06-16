daa18f5a8069b64da0db41e5a3f1de29d53f7298
Gabriel <G_comellas@outlook.es>
2026-06-16 17:18:46 -0300

    Funcionalidades del administrador #18 #19  #50 #51 y tareas del sprint-1 #48 #46

--- js/usuarios.js
+++ js/usuarios.js
@@ -1,37 +1,121 @@
+let bodyTabla;
+window.addEventListener("load", function () {
+   
+  bodyTabla = document.getElementById("bodyTabla");
+  inicializacion();
+  mostrarMovimiento();
+});
+function inicializacion() {
+  const botonValidar = document.getElementById("botonValidar");
+  const botonSesion = document.getElementById("botonSesion")
+  if (botonValidar)
+   {botonValidar.addEventListener("click", validarDatos);}
+  if(botonSesion)
+    {botonSesion.addEventListener("click",inicioSesion);
 
-
-
-let usuariosArray = []
-
-
-let usuarios = {
-    nombre,
-    contraseña,
-    estado,
-    rol,
+  }
 }
 
-const inputNombre = document.getElementById("FloatInputNombre")
-const inputContraseña = document.getElementById("FloatInputContraseña")
+function validarDatos() {
+  const nombreCrear = document.getElementById("inputCrearNombre");
+  const contraseñaCrear = document.getElementById("inputCrearContraseña");
 
-const nombreCrear = document.getElementById("inputCrearNombre")
-const contraseñaCrear = document.getElementById("inputCrearContraseña")
+  const divValidacion = document.getElementById("divValidacion");
 
-function crearUsuario(){
-    let nombre = nombreCrear.value
-    let contraseña = contraseñaCrear.value
 
-    if(nombre.length >= 4){
-        
-    }
+  const nombre = nombreCrear.value;
+  const contraseña = contraseñaCrear.value;
+  const validacion = validarUsuario(nombre, contraseña);
+
+  if (validacion.resultado === true) {
+    agregarUsuarioLocalStorage(crearUsuario(nombre, contraseña));
+    mostrarMovimiento();
+  }
+}
+function validarUsuario(nombre, contraseña) {
+  const cuenta = { resultado: true, fallos: []};
+
+  if (nombre.length < 4) {
+    cuenta["resultado"] = false;
+    cuenta.fallos.push("El nombre de usuario debe tener al menos 4 letras ");
+  }
+  if (contraseña.length<4 || contraseña.length > 10) {
+    cuenta["resultado"] = false;
+    cuenta.fallos.push(
+      "La contraseña del usuario debe tener entre 4 y 10 caracteres",
+    );
+  }
+  return cuenta;
+}
+
+function crearUsuario(nombreUsuario, contraseña) {
+  const usuario =  {
+    nombre: nombreUsuario,
+    contraseña: contraseña,
+    estado: "habilitado",
+    rol: "Usuario",
+  };
+ return usuario;
+}
+function inicioSesion() {
+  const inputNombre = document.getElementById("FloatInputNombre");
+  const inputContraseña = document.getElementById("FloatInputContraseña");
+  const nombreUsuario = inputNombre.value
+  const contraseñaUsuario = inputContraseña.value
+
+  let encontrado = false
+  const usuariosDelArray = JSON.parse(localStorage.getItem("usuarios"));
+
+     for (let i = 0; i <= usuariosDelArray.length - 1; i++) { 
+
+      if(nombreUsuario === usuariosDelArray[i].nombre && contraseñaUsuario === usuariosDelArray[i].contraseña){
+        encontrado = true
+        break;
+      }
+  }
+
+if(encontrado){
+  window.alert("Se encontro")
+}else{
+  window.alert("no se encontro")
+}
+encontrado=false;
 }
 
 
-function inicioSesion(){
-
+function agregarUsuarioLocalStorage(usuario) {
+  const usuariosString = JSON.parse(localStorage.getItem("usuarios")) || [];
+  usuariosString.push(usuario);
+  localStorage.setItem("usuarios", JSON.stringify(usuariosString));
 }
 
-function agregarUsuarioLocalStorage(){
-    const usuariosString = JSON.stringify(usuariosArray)
-    localStorage.setItem("usuarios", usuariosString)
+function mostrarMovimiento() {
+  const usuariosDelArray = JSON.parse(localStorage.getItem("usuarios"));
+
+  bodyTabla.innerHTML = "";
+  for (let i = 0; i <= usuariosDelArray.length - 1; i++) {
+    let indice = i;
+    const tr = document.createElement("tr");
+
+    const td_1 = document.createElement("td");
+    const td_2 = document.createElement("td");
+    const td_3 = document.createElement("td");
+    const td_4 = document.createElement("td");
+    const td_5 = document.createElement("td");
+    const td_6 = document.createElement("td");
+
+    td_1.textContent = usuariosDelArray[i].nombre;
+    td_2.textContent = usuariosDelArray[i].contraseña;
+    td_3.textContent = usuariosDelArray[i].estado;
+    td_4.textContent = usuariosDelArray[i].rol;
+    td_5.innerHTML =
+      '<Button class="btn btn-success mx-2"><i class="bi bi-check-square"></i></Button><Button class="btn btn-danger mx-2"><i class="bi bi-ban"></i></Button><Button class="btn btn-info mx-2"><i class="bi bi-clipboard-x"></i></Button>';
+
+    tr.appendChild(td_1);
+    tr.appendChild(td_2);
+    tr.appendChild(td_3);
+    tr.appendChild(td_4);
+    tr.appendChild(td_5);
+    bodyTabla.appendChild(tr);
+  }
 }
